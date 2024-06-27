0ca94095f5d6a70d7eb2313a3a232ccd8269d3d8
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2024-06-27 04:10:06 -0500

    Cors

---
+++ ReservationService/src/main/java/com/driveshare/ReservationService/infrastructure/config/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package com.driveshare.ReservationService.infrastructure.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer{
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://driveshare-reservationservice.onrender.com/", "http://localhost:4200/", "https://frontend-web-application.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
